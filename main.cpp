6d33fc95e43effd4e0de7992c880c54a600e66e2
sse7ven <siwers17@gmail.com>
2014-01-07 19:39:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,18 +4,17 @@
 
 int main(int argc, char* args[]){
 
-bool quit = false;
 SDL_Window* gWindow = NULL;
 SDL_Event event;
 
 gWindow = SDL_CreateWindow("Nazwa", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
 Menu menu(gWindow, "hello.bmp");
 
-while(!quit)
+while(true)
 {
     if(SDL_PollEvent(&event))
     {
-        quit = menu.onMouseClick(event);
+        menu.onMouseClick(event);
     }
 }
 

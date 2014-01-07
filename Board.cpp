d2f01e2681df32e93fffb5d6753584e29353919b
Gancus <gancus6@gmail.com>
2014-01-07 22:48:53 +0100

    Board wstepnie

---
+++ Board.cpp
@@ -0,0 +1,333 @@
+#include "Board.h"
+
+Board::Board() 
+{
+	tilesFactory = new TilesFactory();
+	previouslyClickedTile = NULL;
+	for (int i = 0; i < 8; i++) 
+	{
+		for(int j = 0; j < 8; j++)
+		{
+			tilesToDestroy[i][j] = false;
+		}
+	}
+	fillBoard();
+	boardSurface = SDL_LoadBMP("board.bmp");
+}
+
+Board::~Board() 
+{
+	delete tilesFactory;
+	for (int i = 0; i < 8; i++) 
+	{
+		for(int j = 0; j < 8; j++)
+		{
+			delete tiles[i][j];
+		}
+	}
+}
+
+void Board::fillBoard() 
+{
+	for(int i = 0; i < 8; i++)
+	{
+		for(int j = 0; j < 8; j++)
+		{
+			bool isValidTile;
+			do
+			{
+				isValidTile = true;
+				tiles[i][j] = tilesFactory -> createTile();
+				if(!isValidTileHorizontal(i, j))
+				{
+					isValidTile = false;
+				}
+				else if(!isValidTileVertical(i, j))
+				{
+					isValidTile = false;
+				}
+				if(!isValidTile)
+				{
+					delete tile[i][j];
+					tile[i][j] = NULL;
+				}
+			}
+			while(!isValidTile)
+		}
+	}
+	
+}
+
+bool Board::isValidTileHorizntal(int row, int column) 
+{
+	if(column < 2)
+	{
+		return true;
+	}
+	if(*(tiles[row][column]) == *(tiles[row][column - 1]))
+	{
+		if(*(tiles[row][column]) == *(tiles[row][column - 2]))
+		{
+			return false;
+		}
+	}
+	return true;	
+}
+
+bool Board::isValidTileVertical(int row, int column) 
+{
+	if(row < 2)
+	{
+		return true;
+	}
+	if(*(tiles[row][column]) == *(tiles[row - 1][column]))
+	{
+		if(*(tiles[row][column]) == *(tiles[row - 2][column]))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+void Board::onMouseDown(SDL_Event event) 
+{
+	if(event.type == SDL_MOUSEBUTTONDOWN)
+	{
+		Tile* clickedTile = NULL;
+		int previouslyClickedTileX;
+		int previouslyClickedTileY;
+		int clickedTileX;
+		int clickedTileY;
+		
+		for(int i = 0; i < 8; i++)
+		{
+			for(int j = 0; j < 8; j++)
+			{
+				if(x > tiles[i][j] -> getX() && x < tiles[i][j] -> getX() + tiles[i][j] -> getWidth())
+				{
+					if(y > tiles[i][j] -> getY() && y < tiles[i][j] -> getY() + tiles[i][j] -> getHeight())
+					{
+						clickedTile = tiles[i][j];
+						clickedTileX = j;
+						clickedTileY = i;
+					}
+				}
+				if(previouslyClickedTile != NULL)
+				{
+					if(previouslyClickedTile == tiles[i][j])
+					{
+						previouslyClickedTileX = j;
+						previouslyClickedTileY = i;
+					}
+				}
+			}
+		}
+		
+		if(clickedTile != NULL)
+		{
+			if(previouslyClickedTile == NULL)
+			{
+				previouslyClickedTile = clickedTile;
+				clickedTile = NULL;
+			}
+			else
+			{
+				if(previouslyClickedTile != clickedTile)
+				{
+					tryToSwap(previouslyClickedTileX, previouslyClickedTileY, clickedTileX, clickedTileY);
+					previouslyClickedTile = NULL;
+					clickedTile = NULL;
+				}
+			}
+		}
+		else 
+		{
+			previouslyClickedTile = NULL;
+		}
+	}
+}
+
+void Board::tryToSwap(int row1, int column1, int row2, int column2) 
+{
+	direction dir;
+	if(row1 == row2 + 1 && column1 == column2)
+	{
+		dir = up;
+		changePlaceOfTiles(row1, column1, dir);
+	}
+	else if(row1 == row2 - 1 && column1 == column2)
+	{
+		dir = down;
+		changePlaceOfTiles(row1, column1, dir);
+	}
+	else if(row1 == row2 && column1 == column2 + 1)
+	{
+		dir = left;
+		changePlaceOfTiles(row1, column1, dir);
+	}
+	else if(row1 == row2 && column1 == column2 - 1)
+	{
+		dir = right;
+		changePlaceOfTiles(row1, column1, dir);
+	}
+}
+
+
+void Board::changePlaceOfTiles(int row, int column, direction dir) 
+{
+	if(isValidMove(row, column, dir)
+	{
+		if(dir == up)
+		{
+			if(row != 0)
+			{
+				std::swap(tiles[row][column], tiles[row - 1][column]);
+			}
+		}
+		else if(dir == down)
+		{	
+			if(row == 7)
+			{
+				std::swap(tiles[row][column], tiles[row + 1][column]);
+			}
+		}
+		else if(dir == left)
+		{
+			if(column == 0)
+			{
+				std::swap(tiles[row][column], tiles[row][column - 1]);
+			}
+		}
+		else if(dir == right)
+		{
+			if(column == 7)
+			{
+				std::swap(tiles[row][column], tiles[row][column + 1]);
+			}
+		}
+		update();
+	}
+}
+
+void Board::update() 
+{
+	if(checkIfMatch())
+	{
+		destroyTiles();
+		draw();
+		refillWithNewTiles();
+	}
+}
+
+bool Board::checkIfMatch() 
+{
+	checkIfMatchHorizontal();
+	checkIfMatchVertical();
+	for(int i = 0; i < 8; i++)
+	{
+		for(int j = 0; j < 8; j++)
+		{
+			if(tilesToDestroy[i][j])
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+void Board::checkIfMatchHorizontal() 
+{
+	int count = 0;
+	for(int i = 0; i < 8; i++)
+	{
+		for(int j = 1; j < 8; j++)
+		{
+			if(*(tiles[i][j]) == *(tiles[i][j - 1])
+			{
+				count++;
+			}
+			else
+			{
+				if(count >= 2)
+				{
+					for (int k = count; k >= 0; k--)
+					{
+						toDestroy[i][j - 1 - count] = true;
+					}
+				}
+				count = 0;
+			}
+		}
+	}
+}
+	
+void Board::checkIfMatchVertical() 
+{
+	int count = 0;
+	for(int i = 1; i < 8; i++)
+	{
+		for(int j = 0; j < 8; j++)
+		{
+			if(*(tiles[i][j]) == *(tiles[i - 1][j])
+			{
+				count++;
+			}
+			else
+			{
+				if(count >= 2)
+				{
+					for (int k = count; k >= 0; k--)
+					{
+						toDestroy[i - 1 - count][j] = true;
+					}
+				}
+				count = 0;
+			}
+		}
+	}
+}
+
+void Board::destroyTiles() 
+{
+	for(int i = 0; i < 8; i++)
+	{
+		for(int j = 0; j < 8; j++)
+		{
+			if(tilesToDestroy[i][j])
+			{
+				delete tiles[i][j];
+				tiles[i][j] = NULL;
+				tilesToDestroy[i][j] = false;
+			}
+		}
+	}
+}
+
+void Board::refillWithNewTiles() 
+{
+	for(int i = 0; i < 8; i++)
+	{
+		for(int j = 0; j < 8; j++)
+		{
+			if(tiles[i][j] == NULL)
+			{
+				tiles[i][j] = tilesFactory -> createTile();
+			}
+		}
+	}
+	update();
+}
+
+void Board::draw(SDL_Surface* targetSurface) 
+{
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			tiles[i][j] -> draw(boardSurface, tiles[i][j] -> getWidth() * i, tiles[i][j] -> getHeight() * j);
+		}
+	}
+	SDL_BlitSurface(boardSurface, NULL, targetSurface, NULL);
+}
---
+++ Board.h
@@ -0,0 +1,113 @@
+#ifndef BOARD_H
+#define BOARD_H
+#include <SDL.h>
+#include "Tile.h"
+#include "TilesFactory.h"
+/**
+ *  \file Board.h
+ *  \brief Plansza do gry typu Puzzle Quest
+ */
+class Board 
+{
+	private:
+		Tile* tiles[8][8];
+		TilesFactory* tilesFactory;
+		bool tilesToDestroy[8][8];
+		Tile* previouslyClickedTile;
+		enum direction {up, down, left, right};
+		SDL_Surface* boardSurface;
+	private:
+		/**
+		 *  \brief Sprawdza (horyzontalnie) czy nowo dodawana kafelka może być umieszczona na danej pozycji
+		 *  
+		 *  \param [in] row Określa rząd w którym znajduje się kafelka którą chcemy dodać
+		 *  \param [in] column Określa kolumnę w której znajduje się kafelka którą chcemy dodać
+		 *  \return true - jeżeli kafelka nie tworzy w rzędzie 3 kafelek o takim samym kolorze, false - w przeciwnym przypadku
+		 *  
+		 *  \details Jako pomoc dla funkcji fillBoard()
+		 */
+		bool isValidTileHorizntal(int row, int column);
+		/**
+		 *  \brief Sprawdza (wertykalnie) czy nowo dodawana kafelka może być umieszczona na danej pozycji
+		 *  
+		 *  \param [in] row Określa rząd w którym znajduje się kafelka którą chcemy dodać
+		 *  \param [in] column Określa kolumnę w której znajduje się kafelka którą chcemy dodać
+		 *  \return true - jeżeli kafelka nie tworzy w kolumnie 3 kafelek o takim samym kolorze, false - w przeciwnym przypadku
+		 *  
+		 *  \details Jako pomoc dla funkcji fillBoard()
+		 */
+		bool isValidTileVertical(int row, int column);
+		/**
+		 *  \brief Sprawdza czy występują rzędy minimum 3 kafelek o takim samym kolorze.
+		 *  
+		 *  \details Jako pomoc dla funkcji checkIfMatch()
+		 */
+		void checkIfMatchHorizontal();
+		/**
+		 *  \brief Sprawdza czy występują kolumny minimum 3 kafelek o takim samym kolorze.
+		 *  
+		 *  \details Jako pomoc dla funkcji checkIfMatch()
+		 */
+		void checkIfMatchVertical();
+		/**
+		 *  \brief Usuwa kafelki przeznaczone do usunięcia
+		 *  
+		 */
+		void destroyTiles();
+		/**
+		 *  \brief Sprawdza czy dwa kafelki sąsiadują ze sobą, a jeżeli tak to wywołuje funkcję changePlaceOfTiles()
+		 *  
+		 *  \param [in] row1 Rząd w którym znajduje się pierwszy kafelek
+		 *  \param [in] column1 Kolumna w której znajduje się pierwszy kafelek
+		 *  \param [in] row2 Rząd w którym znajduje się drugi kafelek
+		 *  \param [in] column2 Kolumna w której znajduje się drugi kafelek
+		 *  
+		 */
+		void tryToSwap(int row1, int column1, int row2, int column2);
+		/**
+		 *  \brief Zamienia ze sobą dwa kafelki
+		 *  
+		 *  \param [in] row Rząd w którym znajduje się kafelek
+		 *  \param [in] column Kolumna w której znajduje się kafelek
+		 *  \param [in] dir Kierunek w którym kafelek ma być przesunięty
+		 *  
+		 */
+		void changePlaceOfTiles(int row, int column, direction dir);
+		/**
+		 *  \brief Wypełnia planszę kafelkami, pilnując żeby nie wystąpiły 3 takie same obok siebie
+		 *  
+		 */
+		void fillBoard();
+		/**
+		 *  \brief Uzupełnia brakujące kafelki
+		 *  
+		 */
+		void refillWithNewTiles();
+		/**
+		 *  \brief Wskazuje kalfelki do zniszczenia
+		 *  
+		 *  \return true - jeżeli są jakieś kafelki do zniszczenia, false - w przeciwnym przypadku
+		 *  
+		 */
+		bool checkIfMatch();
+		void update();
+	public:
+		Board();
+		~Board();
+		/**
+		 *  \brief Obsługuje kliknięcia myszą na kafelkach.
+		 *  
+		 *  \param [in] event - SDL_Event
+		 *  
+		 *  \details Należy wywoływać z klasy sprawującej kontrolę nad zdarzeniami SDL
+		 */
+		void onMouseDown(SDL_Event event);
+		/**
+		 *  \brief Rysuje plansze do  gry wraz z kafelkami
+		 *  
+		 *  \param [in] targetSurface Powierzchnia na której ma być narysowana plansza
+		 *  
+		 */
+		void draw(SDL_Surface* targetSurface);
+};
+#endif

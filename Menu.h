d711afa846f447da678e811cd45d15d65b0fed14
sse7ven <siwers17@gmail.com>
2014-01-07 19:35:40 +0100

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -5,7 +5,7 @@ class Menu
 public:
     Menu(SDL_Window *mainWindow, std::string destinationOfImage);
     ~Menu();
-    bool onMouseClick(SDL_Event event);
+    void onMouseClick(SDL_Event event);
 private:
     SDL_Surface *windowSurface;
     SDL_Surface *backgroundSurface;
